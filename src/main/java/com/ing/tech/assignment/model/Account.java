34ff437d9e9ab8981a0edb65f37bcb1b9b90d967
smeritoiu <silvia.galusca92@gmail.com>
2020-05-04 13:26:05 +0300

    adding account class

---
+++ src/main/java/com/ing/tech/assignment/model/Account.java
@@ -0,0 +1,38 @@
+package com.ing.tech.assignment.model;
+
+public class Account {
+
+	private long id;
+	private String customerName;
+	private double balance;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	@Override
+	public String toString() {
+		return "Account [id=" + id + ", customerName=" + customerName + ", balance=" + balance + "]";
+	}
+
+}
