b50aac906c0cc2596873bd631328e5a627a176e4
smeritoiu <silvia.galusca92@gmail.com>
2020-05-04 20:29:00 +0300

    adding exception handling for wrong pin

--- src/main/java/com/ing/tech/assignment/service/impl/AccountServiceImpl.java
+++ src/main/java/com/ing/tech/assignment/service/impl/AccountServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ing.tech.assignment.exceptions.PinNotFoundException;
 import com.ing.tech.assignment.model.Account;
 import com.ing.tech.assignment.repository.AccountRepository;
 import com.ing.tech.assignment.service.AccountService;
@@ -14,45 +15,42 @@ public class AccountServiceImpl implements AccountService {
 
 	@Autowired
 	AccountRepository repository;
-	
+
 	@Override
 	public Account findAccountByPIN(int pin) {
-		Optional<Account> findAccountByPIN = repository.findByPin(pin);
-		if(findAccountByPIN.isPresent()) {
-			return findAccountByPIN.get();
-		} //handle exception
-		return null;
+		//to do return only one account?
+		return repository.findByPin(pin)
+				.orElseThrow(() -> new PinNotFoundException("Codul PIN introdus este gresit."));
 	}
 
 	@Override
 	public Account depositMoneyToAccount(Account account) {
 		Optional<Account> findByPin = repository.findByPin(account.getPin());
-		if(findByPin.isPresent()) {
+		if (findByPin.isPresent()) {
 			Account foundedAcount = findByPin.get();
 			foundedAcount.setBalance(foundedAcount.getBalance() + account.getBalance());
 			return repository.save(foundedAcount);
 		}
-		//handle exception 
+		// handle exception
 		return null;
 	}
 
 	@Override
 	public Account withdrawMoneyFromAccount(Account account) {
 		Optional<Account> findByPin = repository.findByPin(account.getPin());
-		if(findByPin.isPresent()) {
+		if (findByPin.isPresent()) {
 			Account foundedAcount = findByPin.get();
-			if(foundedAcount.getBalance() > account.getWithdrawValue()) {
+			if (foundedAcount.getBalance() > account.getWithdrawValue()) {
 				foundedAcount.setBalance(foundedAcount.getBalance() - account.getWithdrawValue());
-				return repository.save(foundedAcount);				
+				return repository.save(foundedAcount);
 			}
-			//to do handle if balance is lower than withdraw
+			// to do handle if balance is lower than withdraw
 		}
-		//to do handle exception
+		// to do handle exception
 		return null;
 	}
 
-	
-	//de sters -  doar pt testat
+	// de sters - doar pt testat
 	@Override
 	public Account save(Account account) {
 		return repository.save(account);
